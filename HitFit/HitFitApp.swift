fe98e32cf350fc91f5ebf7aa3737fa1df484f8e9
Konstantin Antonov <kostya-ant@yandex.ru>
2023-08-21 12:39:44 +0400

    Initial Commit

---
+++ HitFit/HitFitApp.swift
@@ -0,0 +1,17 @@
+//
+//  HitFitApp.swift
+//  HitFit
+//
+//  Created by Константин on 21.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct HitFitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
