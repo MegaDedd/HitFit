788ebffc8d89194d9d82f6a77331da4dbb8b48fc
Konstantin Antonov <kostya-ant@yandex.ru>
2023-08-21 13:14:01 +0400

    set up paeging TabView

--- HitFit/View/ContentView.swift
+++ HitFit/View/ContentView.swift
@@ -9,13 +9,17 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        
+        TabView {
+            WelcomeView()
+            
+            ForEach(0..<4) { index in
+                ExerciseView(index: index)
+            }
         }
-        .padding()
+        .tabViewStyle(PageTabViewStyle())
+        .indexViewStyle(
+            PageIndexViewStyle(backgroundDisplayMode: .never))
     }
 }
 
---
+++ HitFit/View/ExerciseView.swift
@@ -0,0 +1,25 @@
+//
+//  ExerciseView.swift
+//  HitFit
+//
+//  Created by Константин on 21.08.2023.
+//
+
+import SwiftUI
+
+struct ExerciseView: View {
+    
+    let videoNames = ["squat", "step-up", "burpee", "sun-salute"]
+    let exerciseNames = ["Squat", "Step Up", "Burpee", "Sun Salute"]
+    let index: Int
+    
+    var body: some View {
+        Text(exerciseNames[index])
+    }
+}
+
+struct ExerciseView_Previews: PreviewProvider {
+    static var previews: some View {
+        ExerciseView(index: 0)
+    }
+}
