33fc866c0435df55a0e7061014143a82701f1f87
Konstantin Antonov <kostya-ant@yandex.ru>
2023-08-23 01:36:26 +0400

    Добавлено отображение modal view с binding к выбранному окну

--- HitFit/View/SuccessView.swift
+++ HitFit/View/SuccessView.swift
@@ -8,13 +8,40 @@
 import SwiftUI
 
 struct SuccessView: View {
+    
+    @Environment(\.dismiss) var dismiss
+    @Binding var selectedTab: Int
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        VStack {
+            
+          Image(systemName: "hand.raised.fill")
+            .resizedToFill(width: 75, height: 75)
+            .foregroundColor(.purple)
+            
+          Text("High Five!")
+            .font(.largeTitle)
+            .fontWeight(.bold)
+            
+          Text("""
+            Good job completing all four exercises!
+            Remember tomorrow's another day.
+            So eat well and get some rest.
+            """)
+          .foregroundColor(.gray)
+            .multilineTextAlignment(.center)
+            
+            Button("Continue") {
+                dismiss()
+                selectedTab = 9
+            }
+        }
     }
 }
 
 struct SuccessView_Previews: PreviewProvider {
     static var previews: some View {
-        SuccessView()
+        SuccessView(selectedTab: .constant(3))
     }
 }
