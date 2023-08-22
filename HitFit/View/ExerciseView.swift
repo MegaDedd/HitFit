8ed95316148f08c7b87b9005913840efae3fc027
Konstantin Antonov <kostya-ant@yandex.ru>
2023-08-23 01:44:38 +0400

    Добавлен вид рейтинга с логикой

--- HitFit/View/ExerciseView.swift
+++ HitFit/View/ExerciseView.swift
@@ -14,13 +14,38 @@ struct ExerciseView: View {
     var exercise: Exercise { Exercise.exercises[index] }
     let interval: TimeInterval = 30
     
+    @State private var rating = 0
+    @State private var showSuccess: Bool = false
+    @Binding var selectedTab: Int
+
+    var startButton: some View {
+        Button("start exercise") { }
+    }
+    
+    var lastExercise: Bool { // явл ли последним упраж
+        index + 1 == Exercise.exercises.count
+    }
+    
+    var buttonDone: some View {
+        Button("done") {
+            if lastExercise {
+                showSuccess.toggle()
+            } else {
+                selectedTab += 1
+            }
+            
+        }
+    }
+    
+    
+    
     var body: some View {
         
         GeometryReader { geometry in
             
             VStack {
                 
-                HeaderView(TitleText: exercise.exerciseName)
+                HeaderView(selectedTab: $selectedTab, titleText: exercise.exerciseName)
                     .padding(.bottom)
 
                 VideoPlayerView(videoName: exercise.videoName)
@@ -29,12 +54,18 @@ struct ExerciseView: View {
                 Text(Date().addingTimeInterval(interval), style: .timer)
                   .font(.system(size: geometry.size.height * 0.07))
                 
+                HStack(spacing: 150) {
+                    startButton
+                    buttonDone
+                        .sheet(isPresented: $showSuccess) {
+                            SuccessView(selectedTab: $selectedTab)
+                                .presentationDetents([.large, .medium])
+                        }
+                }
+                .font(.title3)
+                .padding()
                 
-                Button("Start/Done") { }
-                  .font(.title3)
-                  .padding()
-                
-                RatingView()
+                RatingView(rating: $rating)
                     .padding()
                 
                 Spacer()
@@ -42,13 +73,14 @@ struct ExerciseView: View {
                 Button("History") { }
                   .padding(.bottom)
             }
+
         }
     }
 }
 
 struct ExerciseView_Previews: PreviewProvider {
     static var previews: some View {
-        ExerciseView(index: 0)
+        ExerciseView(index: 3, selectedTab: .constant(3))
     }
 }
 
