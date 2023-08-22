1e43aa6a204afda282f5bd0b7d9174d809ab14ba
Konstantin Antonov <kostya-ant@yandex.ru>
2023-08-22 14:37:12 +0400

    модальное окно

---
+++ HitFit/View/HistoryView.swift
@@ -0,0 +1,54 @@
+//
+//  HistoryView.swift
+//  HitFit
+//
+//  Created by Константин on 21.08.2023.
+//
+
+import SwiftUI
+
+struct HistoryView: View {
+    
+    let history = HistoryStore()
+    
+    var body: some View {
+        
+        ZStack(alignment: .topTrailing) {
+            
+            Button { } label: {
+                Image(systemName: "xmark.circle")
+            }
+            .font(.title)
+            .padding(.trailing)
+
+            
+            VStack {
+                Text("History")
+                    .font(.title)
+                    .padding()
+                
+                Form {
+                    
+                    ForEach(history.exerciseDays) { day in
+                        
+                        Section {
+                            ForEach(day.exercises, id: \.self) { exercice in
+                                Text(exercice)
+                            }
+                        } header: {
+                            Text(day.date.formatted(as: "d MMM"))
+                                .font(.headline)
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct HistoryView_Previews: PreviewProvider {
+    static var previews: some View {
+        HistoryView()
+            .previewLayout(.sizeThatFits)
+    }
+}
