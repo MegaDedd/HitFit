f67afebc3510a7ad03671f6f36b25a0b96f4b981
Konstantin Antonov <kostya-ant@yandex.ru>
2023-08-23 20:29:47 +0400

    struct изменил на class чтобы инициализировать HistoryStore в enviromentObject

--- HitFit/Model/HistoryStore.swift
+++ HitFit/Model/HistoryStore.swift
@@ -13,15 +13,29 @@ struct ExerciseDay: Identifiable {
   var exercises: [String] = []
 }
 
-struct HistoryStore {
+class HistoryStore: ObservableObject {
     
-    var exerciseDays: [ExerciseDay] = []
+   @Published var exerciseDays: [ExerciseDay] = []
 
     // не появится в компиляции
     init() {
       #if DEBUG
       createDevData()
       #endif
+    }
+    
+    func addDoneExercise(_ exerciseName: String) {
+        
+        let today = Date()
+        
+        if today.isSameDay(as: exerciseDays[0].date) {
+            print("Adding \(exerciseName)")
+            exerciseDays[0].exercises.append(exerciseName)
+        } else {
+            exerciseDays.insert(
+                ExerciseDay(date: today, exercises: [exerciseName]),
+                at: 0)
+        }
     }
     
 }
